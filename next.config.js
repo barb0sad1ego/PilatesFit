e72ef5cfed05fd0fb02775192fa21bb1b7c736b7
barb0sad1ego <diegobarbosa.ads@outlook.com>
2025-04-27 22:31:01 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
-const { i18n } = require('./next-i18next.config');
-
 const nextConfig = {
   reactStrictMode: true,
-  i18n,
-  output: 'standalone',
+  i18n: {
+    locales: ['pt-BR', 'en-US', 'es-ES', 'fr-FR'],
+    defaultLocale: 'pt-BR',
+    localeDetection: false
+  },
+  output: 'standalone'
 }
 
 module.exports = nextConfig
